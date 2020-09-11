5207f97dc105702a81399535562154ac5ddcf38d
mads2202 <56963305+mads2202@users.noreply.github.com>
2020-09-11 10:08:45 +0300

    GRPC SERVER

--- src/main/java/com/malyshev2202/store/StoreApplication.java
+++ src/main/java/com/malyshev2202/store/StoreApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.io.IOException;
+
 @EnableJpaRepositories("com.malyshev2202.store.backend.repo.sqlRepo")
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class StoreApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(StoreApplication.class, args);
+
+
     }
 
 }
